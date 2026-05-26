85873dc243e653797e7f9e9de64a00eedac1e340
Richard <richard.carrasco@unmsm.edu.pe>
2026-05-26 05:26:05 -0500

    feat: integrar auth, mascotas y vacunas en main

--- app/src/main/java/com/dsm/g7/medipet/MainActivity.kt
+++ app/src/main/java/com/dsm/g7/medipet/MainActivity.kt
@@ -4,16 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.*
+import com.dsm.g7.medipet.auth.AuthViewModel
+import com.dsm.g7.medipet.auth.HomeScreen
+import com.dsm.g7.medipet.auth.LoginScreen
+import com.dsm.g7.medipet.auth.SignUpScreen
 import com.dsm.g7.medipet.ui.pets.PetScreen
 import com.dsm.g7.medipet.ui.theme.MediPetTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val authViewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MediPetTheme {
-                PetScreen()
+                val user by authViewModel.user.collectAsState()
+                var showSignUp by remember { mutableStateOf(false) }
+
+                if (user != null) {
+                    PetScreen()
+                } else {
+                    if (showSignUp) {
+                        SignUpScreen(
+                            viewModel = authViewModel,
+                            onNavigateToLogin = { showSignUp = false }
+                        )
+                    } else {
+                        LoginScreen(
+                            viewModel = authViewModel,
+                            onNavigateToSignUp = { showSignUp = true }
+                        )
+                    }
+                }
             }
         }
     }
