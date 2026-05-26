7805a0c3f31b90abd8cc0ecb3cae418c99aee7ab
Richard <richard.carrasco@unmsm.edu.pe>
2026-05-26 04:11:51 -0500

    feat: tema MediPet con colores teal

--- app/src/main/java/com/dsm/g7/medipet/ui/theme/Color.kt
+++ app/src/main/java/com/dsm/g7/medipet/ui/theme/Color.kt
@@ -1,2 +1,11 @@
 package com.dsm.g7.medipet.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val Teal80 = Color(0xFF80CBC4)
+val TealGrey80 = Color(0xFFB2DFDB)
+val Green80 = Color(0xFFA5D6A7)
+
+val Teal40 = Color(0xFF0D6E6E)
+val TealGrey40 = Color(0xFF4DB6AC)
+val Green40 = Color(0xFF2E7D32)
